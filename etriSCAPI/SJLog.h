6b6dbce2651c02fec6f1df5f73fd1bda4f6104c5
sjpark <sjpark0@gmail.com>
2025-04-05 16:51:24 +0900

    매크로 수정

--- etriSCAPI/SJLog.h
+++ etriSCAPI/SJLog.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "SJCUDACommon.h"
 
-#define LOGGING(level, format, ...) SJLog::PrintLOG(level, __FILE__, __func__, __LINE__, format, __VA_ARGS__)
+#define LOGGING(level, format, ...) SJLog::PrintLOG(level, __FILE__, __func__, __LINE__, format, ##__VA_ARGS__)
 enum class LOG_LEVEL {
 	NOTHING = 0,
 	WARNING = 1,
