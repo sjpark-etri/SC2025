15385cf4dcdda2058ecb3668fd9135f022826e2b
sjpark <sjpark0@gmail.com>
2025-04-05 20:14:57 +0900

    opencv헤더파일 수정

--- etriSCAPI/SJPlenopticPacker.cpp
+++ etriSCAPI/SJPlenopticPacker.cpp
@@ -1,9 +1,7 @@
 #include "SJPlenopticPacker.h"
 #include "SJLog.h"
 #include <stdio.h>
-#include "opencv2/imgcodecs.hpp"
-#include "opencv2/highgui.hpp"
-#include "opencv2/imgproc.hpp"
+#include "opencv2/opencv.hpp"
 using namespace std;
 using namespace cv;
 
