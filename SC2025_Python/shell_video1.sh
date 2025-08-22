1ecd15e7a07fa6b451034b6c1f4d04da59a0cd16
sjpark0 <sjpark0@gmail.com>
2025-08-22 15:03:22 +0900

    0.1.4버전 수정 4

--- SC2025_Python/shell_video1.sh
+++ SC2025_Python/shell_video1.sh
@@ -8,5 +8,5 @@ do
     docker run --gpus all -it --rm -v ../VideoData2/VideoSample:/etri_workspace/Data etri_scapi:0.1.4 python run_build_layer.py --factor 4 --frame_number $i
 done
 
-docker run --gpus all -it --rm -v ../VideoData2/VideoSample:/etri_workspace/Data -v ../VideoData2/Output:/etri_workspace/Output etri_scapi:0.1.4 python run_render.py --view_range 1.0 --focal 50 --num_views 49 --frame_number 0 --save_viewing
-docker run --gpus all -it --rm -v ../VideoData2/VideoSample:/etri_workspace/Data -v ../VideoData2/Output:/etri_workspace/Output etri_scapi:0.1.4 python run_render_quilt.py --view_range 1.0 --focal 50 --rows 7 --cols 7 --frame_number 0
+docker run --gpus all -it --rm -v ../VideoData2/VideoSample:/etri_workspace/Data -v ../VideoData2/Output:/etri_workspace/Output etri_scapi:0.1.4 python run_render.py --view_range 1.0 --focal 50 --num_views 49 --frame_number 50 --save_viewing
+docker run --gpus all -it --rm -v ../VideoData2/VideoSample:/etri_workspace/Data -v ../VideoData2/Output:/etri_workspace/Output etri_scapi:0.1.4 python run_render_quilt.py --view_range 1.0 --focal 50 --rows 7 --cols 7 --frame_number 50
